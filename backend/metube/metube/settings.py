167a3c8cb23a15467cd5730fc441945ff0105516
frisch <andreas.frisch@leoilab.com>
2018-08-26 16:56:05 +0200

    Ignore intra-server security

--- backend/metube/metube/settings.py
+++ backend/metube/metube/settings.py
@@ -16,7 +16,6 @@ from metube.secret_settings import _SECRET_KEY, _DEBUG, _ALLOWED_HOSTS
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
