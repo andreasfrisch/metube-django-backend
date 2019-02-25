4a17b871fb7b662ad752560ab986207c13435032
frisch <andreas.frisch@leoilab.com>
2019-02-25 12:15:22 +0100

    add API documentation mock

--- metube/src/metube/authentication/api_urls.py
+++ metube/src/metube/authentication/api_urls.py
@@ -3,9 +3,11 @@ URL handlers for authentication API
 """
 
 from django.urls import path
+from django.views.generic import TemplateView
 from metube.authentication.api_views import login, logout, verify
 
 urlpatterns = [
+    path('docs/', TemplateView.as_view(template_name='auth_api.html')),
     path('login/', login),
     path('logout/', logout),
     path('verify/', verify),
--- metube/src/metube/settings.py
+++ metube/src/metube/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'metube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'docs')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
