9ff13cdc398fe49529cb045eff9ba2098766bc7a
Andreas Frisch <andreas.frisch@gmail.com>
2020-03-04 22:01:43 +0100

    updated django version

--- metube/src/metube/blog/models.py
+++ metube/src/metube/blog/models.py
@@ -30,7 +30,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    @models.permalink
     def get_absolute_url(self):
         """
         Generate canonical URL for Post object
